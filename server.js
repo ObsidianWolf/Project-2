e752e93d5a5280ab40124f38cb924860f39ee607
Liam Murray <lmurray298@Liams-MacBook-Pro.local>
2019-04-01 19:30:14 -0400

    added index

--- server.js
+++ server.js
@@ -1,7 +1,31 @@
-var express = require("express");
-var path = require("path");
+var express = require('express');
+var path = require('path');
+var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+var app = require('express')();
 var db = require("./models");
 
-var app = express;
 
-app.use(express.static(path.join(_dirname, "public")));
+var app= express();
+
+
+app.use(express.static(path.join(__dirname, "public")));
+
+var PORT = process.env.PORT || 3306;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var routes = require('./controllers/movie_controller.js');
+app.use('/', routes);
+
+
+db.sequelize.sync().then(function() {
+    app.listen(PORT, function() {
+        console.log('listening on port ' + PORT);
+    });
+});
+
+
+
+
