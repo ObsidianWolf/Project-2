768f64bcb37f450cb13a236e386ff8e29a4583c0
Liam Murray <lmurray298@Liams-MacBook-Pro.local>
2019-04-23 13:10:36 -0400

    push fix

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,22 +3,22 @@ var db = require("../models");
 module.exports = function(app) {
   // Get all examples
   app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+    db.Movie.findAll({}).then(function(Movie) {
+      res.json(Movie);
     });
   });
 
   // Create a new example
   app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+    db.Movie.create(req.body).then(function(Movie) {
+      res.json(Movie);
     });
   });
 
   // Delete an example by id
   app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+    db.Movie.destroy({ where: { id: req.params.id } }).then(function(Movie) {
+      res.json(Movie);
     });
   });
 };
