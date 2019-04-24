0caaaa2e4d30ff5ac17cdd0464d5b1ec35a7504d
Your Name <you@example.com>
2019-04-23 22:20:48 -0400

    copied some css from clone in local

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
     db.Movie.findAll({}).then(function(Movie) {
-      res.render("index", {
+      res.render("movie", {
         msg: "Welcome!",
         examples: Movie
       });
