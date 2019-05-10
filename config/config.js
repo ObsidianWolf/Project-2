3a0159e0b6b1a569fee48e30db9f9ae81dfcf8ef
Your Name <you@example.com>
2019-05-09 21:43:05 -0400

    changed to sequalize

--- config/config.js
+++ config/config.js
@@ -1,44 +1,42 @@
-const mysql= require('mysql');
-var connection;
+// const mysql= require('mysql');
+// var connection;
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-}else{
-  connection = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'hacktheplanet',
-    database:'todoagain_db'
-  });
-};
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// }else{
+//   connection = mysql.createConnection({
+//     host:'localhost',
+//     user:'root',
+//     password:'Root',
+//     database:'movies_db'
+//   });
+// };
 
-connection.connect();
-module.exports = connection;
-
-
-// require("dotenv").config();
+// connection.connect();
+// module.exports = connection;
 
 
-// module.exports = {
-//   "development": {
-//       "username": "root",
-//       "password": "Root",
-//       "database": "movies_db",
-//       "host": "127.0.0.1",
-//       "dialect": "mysql"
-//   },
-//   "test": {
-//       "username": "root",
-//       "password": "Root",
-//       "database": "movies_db",
-//       "host": "localhost",
-//       "dialect": "mysql",
-//       "logging": false
-//     },
-//     "production": {
-//       "use_env_variable": "JAWSDB_URL",
-//       "dialect": "mysql"
-//     }
-//   }
-
-
+module.exports = {
+    "development": {
+          "username": "root",
+          "password": "Root",
+          "database": "movies_db",
+          "host": "127.0.0.1",
+          "dialect": "mysql"
+      },
+      "test": {
+            "username": "root",
+            "password": "Root",
+            "database": "movies_db",
+            "host": "localhost",
+            "dialect": "mysql",
+            "logging": false
+          },
+          "production": {
+              "use_env_variable": "JAWSDB_URL",
+              "dialect": "mysql"
+            }
+          }
+        
+        
+        
