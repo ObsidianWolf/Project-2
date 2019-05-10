b0f5520ee6e4748041d0cfa97b2f1f34ec65b345
Your Name <you@example.com>
2019-05-09 21:50:04 -0400

    changed 404 to not found

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -28,6 +28,6 @@ module.exports = function (app) {
 
   // Render 404 page for any unmatched routes
   app.get("*", function (req, res) {
-    res.render("404");
+    res.render("notFound");
   });
 };
